3ba6672a4b62fdd8075248e56249221aaff33267
bushiqbw <1036279449@qq.com>
2025-01-29 23:44:45 +0800

    Init

---
+++ src/main/java/Stream_InputOutput/输入输出流模板.java
@@ -0,0 +1,28 @@
+package Stream_InputOutput;
+
+import java.util.*;
+import java.io.*;
+public class 输入输出流模板 {
+    static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+    static int nextInt() throws IOException {
+        st.nextToken();
+        return (int)st.nval;
+    }
+    static double nextDouble() throws IOException {
+        st.nextToken();
+        return st.nval;
+    }
+    static String nextString() throws IOException {
+        st.nextToken();
+        return st.sval;
+    }
+
+    public static void main(String[] args) throws IOException {
+        //输出
+        pw.println();
+        pw.flush();
+    }
+
+
+}
