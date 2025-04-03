804d9618f4f3411e8eaaff2c20f1c045c9ab737a
bushiqbw <1036279449@qq.com>
2025-04-03 19:15:06 +0800

    1111

--- src/main/java/二分/Main.java
+++ src/main/java/二分/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 import java.util.*;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,46 +16,42 @@ public class Main {
                 matrix[i][j] = scanner.nextInt();
             }
         }
+        System.out.println(minOperations(matrix));
+    }
 
+    public static int minOperations(int[][] matrix) {
+        int n = matrix.length;
+        int totalOperations = 0;
         boolean[][] visited = new boolean[n][n];
-        int total = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (!visited[i][j]) {
-                    List<int[]> group = new ArrayList<>();
+                    List<Integer> group = new ArrayList<>();
                     int x = i;
                     int y = j;
-                    do {
-                        group.add(new int[]{x, y});
-                        int[] next = rotate90(x, y, n);
-                        x = next[0];
-                        y = next[1];
-                    } while (x != i || y != j);
-
-                    for (int[] pos : group) {
-                        visited[pos[0]][pos[1]] = true;
+                    for (int k = 0; k < 4; k++) {
+                        group.add(matrix[x][y]);
+                        visited[x][y] = true;
+                        int tempX = y;
+                        int tempY = n - 1 - x;
+                        x = tempX;
+                        y = tempY;
                     }
-
-                    List<Integer> values = new ArrayList<>();
-                    for (int[] pos : group) {
-                        values.add(matrix[pos[0]][pos[1]]);
-                    }
-
-                    int max = Collections.max(values);
-                    int sum = 0;
-                    for (int val : values) {
-                        sum += (max - val);
-                    }
-                    total += sum;
+                    totalOperations += minOperationsForGroup(group);
                 }
             }
         }
-
-        System.out.println(total);
+        return totalOperations;
     }
 
-    private static int[] rotate90(int x, int y, int n) {
-        return new int[]{y, n - 1 - x};
+    private static int minOperationsForGroup(List<Integer> group) {
+        Collections.sort(group);
+        int median = group.get(group.size() / 2);
+        int operations = 0;
+        for (int num : group) {
+            operations += Math.abs(num - median);
+        }
+        return operations;
     }
 }
